28fec6e1f42ce1909fe4fcf720d66ab010746940
EdwardJFox <edwardjfox@outlook.com>
2020-01-08 23:25:02 +0000

    Moved the logic for the Windoe's size and position down to the Windoe itself

--- src/windoe/Desktop.tsx
+++ src/windoe/Desktop.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './Desktop.scss';
 import Windoe from './Windoe';
 import Icon from './Icon';
@@ -6,129 +6,47 @@ import Icon from './Icon';
 interface DesktopProps {
   height: string;
   width: string;
-  windoes: any;
-  updateWindoes: any;
 }
 
 const Desktop: React.FC<DesktopProps> = (props) => {
-  const [currentlyDragging, setCurrentlyDragging] = useState(0);
-  const [draggingStartPosition, setDraggingStartPosition] = useState({ x: 0, y: 0 });
-  const [previousPosition, setPreviousPosition] = useState({ x: 0, y: 0 });
-
-  const [currentlyResizing, setCurrentlyResizing] = useState(0);
-  const [resizingType, setResizingType] = useState('');
-  const [previousSize, setPreviousSize] = useState({ width: 0, height: 0 });
-
-  function handleMouseDown(e: any, windoe: any) {
-    setCurrentlyDragging(windoe.id);
-    setDraggingStartPosition({ x: e.pageX, y: e.pageY });
-    setPreviousPosition(windoe.position);
-  }
-
-  function handleMouseUp() {
-    setCurrentlyDragging(0);
-    setResizingType('');
-  }
-
-  function handleResizeClick(e: any, windoe: any, resizeType: string) {
-    setResizingType(resizeType);
-    setCurrentlyResizing(windoe.id);
-    setDraggingStartPosition({ x: e.pageX, y: e.pageY });
-    setPreviousSize(windoe.size);
-    setPreviousPosition(windoe.position);
-  }
-
-  function handleMouseMove(e: any) {
-    if(currentlyDragging > 0) {
-      handleDragging(e);
-    }
-    if(currentlyResizing > 0) {
-      handleResize(e);
-    }
-  }
-
-  function handleDragging(e: any) {
-    let dragging = props.windoes.find((windoe: any) => windoe.id == currentlyDragging);
-    let updatedPosition = { x: 0, y: 0 };
-    updatedPosition.x = previousPosition.x + (e.pageX - draggingStartPosition.x);
-    updatedPosition.y = previousPosition.y + (e.pageY - draggingStartPosition.y);
-    dragging.position = updatedPosition;
-    updateWindoeInState(dragging);
-  }
-
-  function handleResize(e: any) {
-    let resizing = props.windoes.find((windoe: any) => windoe.id == currentlyResizing);
-    let updatedSize = { width: resizing.size.width, height: resizing.size.height };
-    let updatedPosition = { x: resizing.position.x, y: resizing.position.y };
-
-    switch(resizingType){
-      case 'top':
-        updatedSize.height = previousSize.height + (previousPosition.y - e.pageY);
-        updatedPosition.y = previousPosition.y + (e.pageY - draggingStartPosition.y);
-        break;
-      case 'bottom':
-        updatedSize.height = e.pageY - resizing.position.y;
-        break;
-      case 'left':
-        updatedSize.width = previousSize.width + (previousPosition.x - e.pageX);
-        updatedPosition.x = previousPosition.x + (e.pageX - draggingStartPosition.x);
-        break;
-      case 'right':
-        updatedSize.width = e.pageX - resizing.position.x;
-        break;
-    }
-    resizing.size = updatedSize;
-    resizing.position = updatedPosition;
-
-    updateWindoeInState(resizing);
-  }
+  const [windoes, setWindoes] = useState([]);
+  const desktopRef = useRef(null);
 
   function handleFocus(e: any, windoe: any) {
-    const windoeIndex = props.windoes.indexOf(windoe);
-    if(windoeIndex > 0) {
-      let updatedWindoes = [ ...props.windoes ];
-      updatedWindoes.splice(windoeIndex, 1);
-      updatedWindoes.push(windoe);
-      props.updateWindoes(updatedWindoes);
-    }
-  }
-
-  function updateWindoeInState(windoe: any) {
-    const windoeIndex = props.windoes.indexOf(windoe);
-    let updatedWindoes = [ ...props.windoes ];
-    updatedWindoes[windoeIndex] = windoe;
-    props.updateWindoes(updatedWindoes);
+    // const windoeIndex = props.windoes.indexOf(windoe);
+    // if(windoeIndex > 0) {
+    //   let updatedWindoes = [ ...props.windoes ];
+    //   updatedWindoes.splice(windoeIndex, 1);
+    //   updatedWindoes.push(windoe);
+    //   props.updateWindoes(updatedWindoes);
+    // }
   }
 
   function handleIconClick(windoeType: string) {
-    const selectedWindow = props.windoes.find((windoe: any) => windoe.type == windoeType);
-    const windoeIndex = props.windoes.indexOf(selectedWindow);
-    let updatedWindoes = [ ...props.windoes ];
-    updatedWindoes[windoeIndex].state = "opened";
-    props.updateWindoes(updatedWindoes);
+    let updatedWindoes = [...windoes];
+    updatedWindoes.push({ windoeType })
+    setWindoes(updatedWindoes);
   }
 
   function handleWindoeClose(windoeType: any) {
-    const selectedWindow = props.windoes.find((windoe: any) => windoe.type == windoeType);
-    const windoeIndex = props.windoes.indexOf(selectedWindow);
-    let updatedWindoes = [ ...props.windoes ];
-    updatedWindoes[windoeIndex].state = "closed";
-    props.updateWindoes(updatedWindoes);
+    // const selectedWindow = props.windoes.find((windoe: any) => windoe.type == windoeType);
+    // const windoeIndex = props.windoes.indexOf(selectedWindow);
+    // let updatedWindoes = [ ...props.windoes ];
+    // updatedWindoes[windoeIndex].state = "closed";
+    // props.updateWindoes(updatedWindoes);
   }
 
   return (
-    <div className="desktop" style={{ height: props.height, width: props.width }} onMouseMove={handleMouseMove} onMouseUp={handleMouseUp}>
+    <div className="desktop" style={{ height: props.height, width: props.width }} ref={desktopRef}>
       <Icon position={{ x: 10, y: 10 }} icon={`${process.env.PUBLIC_URL}/images/icon_user.png`} title="About" handleOnClick={() => handleIconClick('about')} />
       <Icon position={{ x: 10, y: 90 }} icon="missing" title="previous_work" handleOnClick={() => handleIconClick('previous_work')} />
       <Icon position={{ x: 10, y: 190 }} icon={`${process.env.PUBLIC_URL}/images/icon_links.png`} title="Links" handleOnClick={() => handleIconClick('links')} />
-      { props.windoes && props.windoes.map(
-        (windoe: any) => windoe.state === 'opened' && <Windoe
+      { windoes && windoes.map(
+        (windoe: any) => <Windoe
           windoe={windoe}
-          handleToolbarClick={handleMouseDown}
-          handleToolbarRelease={handleMouseUp}
-          handleResizeClick={handleResizeClick}
           handleFocus={handleFocus}
-          handleWindoeClose={handleWindoeClose} />
+          handleClose={handleWindoeClose}
+          desktopRef={desktopRef} />
       )}
     </div>
   );
--- src/windoe/Windoe.tsx
+++ src/windoe/Windoe.tsx
@@ -1,33 +1,110 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './Windoe.scss';
 
 export interface WindoeProps {
   windoe: any;
-  handleToolbarClick: any;
-  handleToolbarRelease: any;
-  handleResizeClick: any;
   handleFocus: any;
-  handleWindoeClose: any;
+  handleClose: any;
+  desktopRef: any;
 }
 
 const Windoe: React.FC<WindoeProps> = (props) => {
+  const [position, setPosition] = useState({ x: 100, y: 100 });
+  const [size, setSize] = useState({ width: 400, height: 300 });
+  
+  const beingDraggedRef = React.useRef(false);
+  const beingResizedRef = React.useRef(false);
+  const resizeTypeRef = React.useRef('');
+  const draggingStartPositionRef = React.useRef({ x: 0, y: 0 });
+  const previousPositionRef = React.useRef(position);
+  const previousSizeRef = React.useRef({ width: 0, height: 0 });
+
+  const handleMouseMove = (e: any) =>{
+    if(beingDraggedRef.current) {
+      handleDragging(e);
+    } else if(beingResizedRef.current) {
+      handleResize(e);
+    }
+  }
+
+  function handleToolbarClick(e: any) {
+    beingDraggedRef.current = true;
+    draggingStartPositionRef.current = ({ x: e.pageX, y: e.pageY });
+    previousPositionRef.current = position;
+  }
+
+  function handleResizeClick(e: any, resizeType: string) {
+    beingResizedRef.current = true;
+    resizeTypeRef.current = resizeType;
+    draggingStartPositionRef.current = ({ x: e.pageX, y: e.pageY });
+    previousSizeRef.current = size;
+    previousPositionRef.current = position;
+  }
+
+  function handleMouseRelease() {
+    beingDraggedRef.current = false;
+    beingResizedRef.current = false;
+  }
+
+  function handleDragging(e: any) {
+    let updatedPosition = { ...position };
+
+    updatedPosition.x = previousPositionRef.current.x + (e.pageX - draggingStartPositionRef.current.x);
+    updatedPosition.y = previousPositionRef.current.y + (e.pageY - draggingStartPositionRef.current.y);
+    setPosition(updatedPosition);
+  }
+
+  function handleResize(e: any) {
+    let updatedSize = { ...size };
+    let updatedPosition = { ...position };
+
+    switch(resizeTypeRef.current){
+      case 'top':
+        updatedSize.height = previousSizeRef.current.height + (previousPositionRef.current.y - e.pageY);
+        updatedPosition.y = previousPositionRef.current.y + (e.pageY - draggingStartPositionRef.current.y);
+        break;
+      case 'bottom':
+        updatedSize.height = e.pageY - position.y;
+        break;
+      case 'left':
+        updatedSize.width = previousSizeRef.current.width + (previousPositionRef.current.x - e.pageX);
+        updatedPosition.x = previousPositionRef.current.x + (e.pageX - draggingStartPositionRef.current.x);
+        break;
+      case 'right':
+        updatedSize.width = e.pageX - position.x;
+        break;
+    }
+
+    setPosition(updatedPosition);
+    setSize(updatedSize);
+  }
+
+  useEffect(() => {
+    props.desktopRef.current.addEventListener('mousemove', handleMouseMove);
+    props.desktopRef.current.addEventListener('mouseup', handleMouseRelease);
+
+    return () => {
+      props.desktopRef.current.removeEventListener('mousemove', handleMouseMove);
+      props.desktopRef.current.removeEventListener('mouseup', handleMouseRelease);
+    }
+  }, [props.desktopRef, position, size]);
+
   return (
     <div className="windoe" style={{
-      left: `${props.windoe.position.x}px`,
-      top: `${props.windoe.position.y}px`,
-      width: `${props.windoe.size.width}px`,
-      height: `${props.windoe.size.height}px` }}
+      left: `${position.x}px`,
+      top: `${position.y}px`,
+      width: `${size.width}px`,
+      height: `${size.height}px` }}
       onMouseDown={(e) => props.handleFocus(e, props.windoe)}>
 
-      <div className="windoeResize top" onMouseDown={(e) => props.handleResizeClick(e, props.windoe, 'top')}></div>
-      <div className="windoeResize bottom" onMouseDown={(e) => props.handleResizeClick(e, props.windoe, 'bottom')}></div>
-      <div className="windoeResize left" onMouseDown={(e) => props.handleResizeClick(e, props.windoe, 'left')}></div>
-      <div className="windoeResize right" onMouseDown={(e) => props.handleResizeClick(e, props.windoe, 'right')}></div>
+      <div className="windoeResize top" onMouseDown={(e) => handleResizeClick(e, 'top')}></div>
+      <div className="windoeResize bottom" onMouseDown={(e) => handleResizeClick(e, 'bottom')}></div>
+      <div className="windoeResize left" onMouseDown={(e) => handleResizeClick(e, 'left')}></div>
+      <div className="windoeResize right" onMouseDown={(e) => handleResizeClick(e, 'right')}></div>
 
       <div className="windoeToolbar"
-        onMouseDown={(e) => props.handleToolbarClick(e, props.windoe)}
-        onMouseUp={props.handleToolbarRelease}>
-        <div className="windoeCloseButton" onMouseUp={() => props.handleWindoeClose(props.windoe.type)}>Close</div>
+        onMouseDown={handleToolbarClick}>
+        <div className="windoeCloseButton" onMouseUp={() => props.handleClose(props.windoe.type)}>Close</div>
       </div>
       <div className="windoeContent">{ props.children }</div>
     </div>
